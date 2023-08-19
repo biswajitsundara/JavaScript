61c9e9dac9327f71a172c55de0198ebea3f421bf
Biswajit Sundara <biswajitsundara@gmail.com>
2023-08-19 21:11:32 +0530

    proto type

---
+++ src/43. Prototype/Basics/index.js
@@ -0,0 +1,13 @@
+const user = {
+    name: 'Biswajit'
+}
+
+const hobbies = ['eat', 'play', 'read'];
+
+function greet(){
+    console.log('Hello');
+}
+
+const username = 'Mark';
+
+console.log(Object.getPrototypeOf(user));
